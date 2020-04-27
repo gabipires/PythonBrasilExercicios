caa188c9ecb9fc113a6b13348eb6419b12b52645
Gabi Pires <gabrielatrindadepires@gmail.com>
2020-04-26 21:31:53 -0300

    Exercicios Estrutura Decisao

---
+++ 01.Estrutura_Sequencial/05.metros_centimetros.py
@@ -0,0 +1,4 @@
+centimetros=int(input("Informe a medida em centímetros:"))
+metros=centimetros*100
+
+print(centimetros,"cm equivalem a", metros,"m")
---
+++ 01.Estrutura_Sequencial/06.raio_circulo.py
@@ -0,0 +1,5 @@
+raio=int(input("Informe o raio da circunferência:"))
+pi=3.14
+area=pi*raio**2
+
+print("A área da circunferência de raio", raio, "é:", area)
